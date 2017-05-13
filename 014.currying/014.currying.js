8ecd687660e1b9c6e7da903a7f53f62c5c54fce7
Jason Shin <visualbbasic@gmail.com>
2017-05-14 09:55:53 +1000

    currying

---
+++ 014.currying/014.currying.js
@@ -0,0 +1,15 @@
+const add = x => y => x + y
+
+const inc = add(1)
+
+const res = inc(2)
+
+console.log(res)
+
+const modulo = dvr => dvd => dvd % dvr
+
+const isOdd = modulo(2)
+
+const res2 = isOdd(22)
+
+console.log(res2)
