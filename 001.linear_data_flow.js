006494209f1f20343c54123785efac04b8c3214c
Jason Shin <visualbbasic@gmail.com>
2017-05-13 11:28:20 +1000

    boxed

--- 001.linear_data_flow.js
+++ 001.linear_data_flow.js
@@ -1,8 +1,14 @@
-const nextCharForNumberString = (str) => {
+/*const nextCharForNumberString = (str) => {
   const trimmed = str.trim()
   const number = parseInt(trimmed)
   const nextNumber = number + 1
   return String.fromCharCode(nextNumber)
-}
+}*/
 
+const nextCharForNumberString = (str) =>
+  [str]
+    .map(s => s.trim())
+    .map(trimmed => parseInt(trimmed))
+    .map(number => number + 1)
+    .map(nextNumber => String.fromCharCode(nextNumber))
 console.log(nextCharForNumberString('  65  '))
