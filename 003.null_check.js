b2a8a753e62b962f2141edd0d91967099cd3bbb1
Jason Shin <visualbbasic@gmail.com>
2017-05-13 12:21:42 +1000

    nullable check in one go

--- 003.null_check.js
+++ 003.null_check.js
@@ -10,14 +10,21 @@ const Left = x => ({
   inspect: () => `Left(${x})`
 })
 
+const fromNullable = x =>
+  x != null ? Right(x) : Left(null)
+
+/*
 const findColour = name => {
   const found = ({red: '#ff4444', blue: '#3b5678', yellow: '#fff68f'})[name]
   return found ? Right(found) : Left(null)
 }
+*/
+const findColour = name =>
+  fromNullable({red: '#ff4444', blue: '#3b5678', yellow: '#fff68f'}[name])
 
 // const result = Left(2).map(x => x + 1).map(x => x / 2).fold(x => 'error', x => x)
 const result =
-  findColour('yo')
+  findColour('red')
     .map(c => c.slice(1))
     .fold(e => 'no colour found', c => c.toUpperCase())
 
