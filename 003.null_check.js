90061e4747fc8c60c8f99db747d7a6d653da91f9
Jason Shin <visualbbasic@gmail.com>
2017-05-13 12:18:29 +1000

    colour null check

--- 003.null_check.js
+++ 003.null_check.js
@@ -10,6 +10,15 @@ const Left = x => ({
   inspect: () => `Left(${x})`
 })
 
+const findColour = name => {
+  const found = ({red: '#ff4444', blue: '#3b5678', yellow: '#fff68f'})[name]
+  return found ? Right(found) : Left(null)
+}
 
-const result = Left(2).map(x => x + 1).map(x => x / 2).fold(x => 'error', x => x)
+// const result = Left(2).map(x => x + 1).map(x => x / 2).fold(x => 'error', x => x)
+const result =
+  findColour('yo')
+    .map(c => c.slice(1))
+    .fold(e => 'no colour found', c => c.toUpperCase())
+
 console.log(result)
