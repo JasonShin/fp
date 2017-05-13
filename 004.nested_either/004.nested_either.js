4748161f6a626b93bcdeb7af55f6d9b469fab2f9
Jason Shin <visualbbasic@gmail.com>
2017-05-13 12:27:11 +1000

    try catch

---
+++ 004.nested_either/004.nested_either.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const Right = x => ({
+  map: f => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Right(${x})`
+})
+
+const Left = x => ({
+  map: f => Left(x),
+  fold: (f, g) => f(x),
+  inspect: () => `Left(${x})`
+})
+
+const fromNullable = x =>
+  x != null ? Right(x) : Left(null)
+
+const tryCatch = f => {
+  try {
+    return Right(f())
+  } catch (e) {
+    return Left(e)
+  }
+}
+
