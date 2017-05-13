105a30961b980ab926e2fce6f9c59539450d42ae
Jason Shin <visualbbasic@gmail.com>
2017-05-13 11:41:08 +1000

    percent 2 float

--- 002.composable_functions.js
+++ 002.composable_functions.js
@@ -13,3 +13,15 @@ const moneyToFloat = str =>
   Box(str)
   .map(s => s.replace(/\$/g, ''))
   .fold(s => parseFloat(s))
+
+/*
+const percentToFloat = str => {
+  const replaced = str.replace(/\%/g, '')
+  const number = parseFloat(replaced)
+  return number * 0.01
+}
+*/
+const percentToFloat = str =>
+  Box(s => s.replace(/\%/g, ''))
+  .map(replaced => parseFloat(replaced))
+  .fold(number => number * 0.01)
