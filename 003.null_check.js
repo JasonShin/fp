371a4d3eb180d6043faf7f597dae1693496950f2
Jason Shin <visualbbasic@gmail.com>
2017-05-13 12:12:41 +1000

    functional nullcheck

---
+++ 003.null_check.js
@@ -0,0 +1,15 @@
+const Right = x => ({
+  map: f => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Right(${x})`
+})
+
+const Left = x => ({
+  map: f => Left(x),
+  fold: (f, g) => f(x),
+  inspect: () => `Left(${x})`
+})
+
+
+const result = Left(2).map(x => x + 1).map(x => x / 2).fold(x => 'error', x => x)
+console.log(result)
