db247c0a88abf1d5e9789af5e9b71bbc3a12f75b
Jason Shin <visualbbasic@gmail.com>
2017-05-13 17:37:45 +1000

    sum

--- 007.fail_safe_with_monoids/007.fail_safe_with_monoids.js
+++ 007.fail_safe_with_monoids/007.fail_safe_with_monoids.js
@@ -9,5 +9,10 @@ const All = x =>
 
 All.empty = () => All(true)
 
+const sum = xs =>
+  xs.reduce((acc, x) => acc + x, 0)
+
 const res = All(true).concat(All(true).concat(All.empty()))
 console.log(res);
+
+console.log(sum([1,2,3]))
