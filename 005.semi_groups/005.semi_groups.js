48a8ee76c997cc445c52313162fe977470aef071
Jason Shin <visualbbasic@gmail.com>
2017-05-13 13:07:06 +1000

    boolean semi group

--- 005.semi_groups/005.semi_groups.js
+++ 005.semi_groups/005.semi_groups.js
@@ -6,5 +6,16 @@ const Sum = x => ({
     `Sum(${x})`
 })
 
+const All = x => ({
+  x,
+  concat: ({x: y}) =>
+    All(x && y),
+  inspect: () =>
+    `All(${x})`
+})
+
 const result = Sum(1).concat(Sum(2))
 console.log(result)
+const result2 = All(true).concat(All(false))
+console.log(result2)
+
