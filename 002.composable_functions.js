a6f52aa12573dc1d71f2760804f76c16fd31a9c3
Jason Shin <visualbbasic@gmail.com>
2017-05-13 12:05:24 +1000

    apply discount

--- 002.composable_functions.js
+++ 002.composable_functions.js
@@ -11,9 +11,8 @@ const moneyToFloat = str =>
 */
 // Note: replace fold to map because I'm going to map it over inside applyDiscount
 const moneyToFloat = str =>
-  Box(str)
-  .map(s => s.replace(/\$/g, ''))
-  .map(s => parseFloat(s))
+  Box(str.replace(/\$/g, ''))
+  .map(r => parseFloat(r))
 
 /*
 const percentToFloat = str => {
@@ -25,6 +24,23 @@ const percentToFloat = str => {
 
 // Note: replace fold to map because I'm going to map it over inside applyDiscount
 const percentToFloat = str =>
-  Box(s => s.replace(/\%/g, ''))
+  Box(str.replace(/%/g, ''))
   .map(replaced => parseFloat(replaced))
   .map(number => number * 0.01)
+/*
+const applyDiscount = (price, discount) => {
+  const cost = moneyToFloat(price)
+  const savings = percentToFloat(discount)
+  return cost - cost * savings
+}
+*/
+const applyDiscount = (price, discount) =>
+  moneyToFloat(price)
+    .fold(cost =>
+      percentToFloat(discount)
+        .fold(savings =>
+          cost - cost * savings))
+
+console.log(percentToFloat('20%'))
+const result = applyDiscount('$7.00', '20%')
+console.log(result)
