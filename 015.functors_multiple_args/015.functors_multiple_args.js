673d6570592d9c7b018e129a614bb02c306fdb4b
Jason Shin <visualbbasic@gmail.com>
2017-05-14 10:39:00 +1000

    lift

--- 015.functors_multiple_args/015.functors_multiple_args.js
+++ 015.functors_multiple_args/015.functors_multiple_args.js
@@ -5,11 +5,19 @@ const Box = x =>
     fold: f => f(x),
     inspect: () => `Box(${x})`
   })
+const add = x => y => x + y
 
 const res = Box(x => x + 1).ap(Box(2))
 
 console.log(res)
 
-const res2 = Box(x => y => x + y).ap(Box(2)).ap(Box(3))
+const res2 = Box(add).ap(Box(2)).ap(Box(3))
 
 console.log(res2)
+
+const liftA2 = (f, fx, fy) =>
+  fx.map(f).ap(fy)
+
+const res3 = liftA2(add, Box(2), Box(3))
+
+console.log(res3)
