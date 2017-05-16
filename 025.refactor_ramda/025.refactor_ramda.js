e2df3eed271f538ee44d4bb5a40e2be09e6d7f01
Jason Shin <visualbbasic@gmail.com>
2017-05-16 22:44:54 +1000

    Ramda refactor

---
+++ 025.refactor_ramda/025.refactor_ramda.js
@@ -0,0 +1,13 @@
+const R = require('ramda')
+
+const person = {
+  id: 1,
+  name: 'Joe',
+};
+
+const generateUrl = (id) => `https://img.socialnetwork.com/avatar/${id}.png`
+const getUrlFromPerson = R.compose(generateUrl, R.propOr('default', 'id'))
+const getUpdatedPerson = R.converge(R.assoc('avatar'), [getUrlFromPerson, R.identity])
+
+const result = getUpdatedPerson(person)
+console.log(result)
