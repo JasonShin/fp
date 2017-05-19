35c98a464d2c16c9a6b9b76d82cfbaf19aba7d9f
Jason Shin <visualbbasic@gmail.com>
2017-05-19 21:46:26 +1000

    pipe

---
+++ 033.pipeline/index.js
@@ -0,0 +1,25 @@
+const R = require('ramda')
+
+const teams = [
+  { name: 'Lions', score: 5 },
+  { name: 'Tigers', score: 4 },
+  { name: 'Bear', score: 6 },
+  { name: 'Cats', score: 2 },
+]
+
+/*
+const getTopName =
+  R.compose(
+    R.prop('name'),
+    R.head,
+    R.sort((a,b) => b.score - a.score)
+  )
+*/
+const getTopName = R.pipe(
+  R.sort((a,b) => b.score - a.score),
+  R.head,
+  R.prop('name')
+)
+const result = getTopName(teams)
+
+console.log(result)
