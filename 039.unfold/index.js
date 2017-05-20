3d2264cf90df3f24edeadb6d16d1c85f460e0a19
Jason Shin <visualbbasic@gmail.com>
2017-05-20 15:32:33 +1000

    unfold

--- 039.unfold/index.js
+++ 039.unfold/index.js
@@ -1,5 +1,8 @@
 const R = require('ramda')
 const throughNByOne = R.curry((limit, n) => n > limit ? false : [n, n + 1])
+const throughNBaseTwo = R.curry((limit, n) => n > limit ? false : [n, n * 2])
 const result = R.unfold(throughNByOne(50), 1)
+const result2 = R.unfold(throughNBaseTwo(256), 4)
 
 console.log(result)
+console.log(result2)
